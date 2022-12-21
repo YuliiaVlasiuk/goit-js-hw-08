8d32713613a91dacd9e5a709717a5bbd246e52f2
YuliiaVlasiuk <vlasyukyulia@gmail.com>
2022-12-21 19:51:06 +0200

    test

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,8 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
 
 console.log(galleryItems);
+
+console.log(SimpleLightbox);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,5 @@
+import Player from '@vimeo/player';
+console.log(Player);
 
+test =10;
+console.log(test);
