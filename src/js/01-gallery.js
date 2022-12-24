715a857876f2a0d8ead95689bd8c7b29da77e9ac
YuliiaVlasiuk <vlasyukyulia@gmail.com>
2022-12-24 20:43:38 +0200

    prettier

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,23 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // console.log(galleryItems);
 
-const gallery=document.querySelector('.gallery');
-const arrayOfPictures=createGalleryMarkup(galleryItems);
-gallery.insertAdjacentHTML('beforeend',arrayOfPictures);
+const gallery = document.querySelector('.gallery');
+const arrayOfPictures = createGalleryMarkup(galleryItems);
+gallery.insertAdjacentHTML('beforeend', arrayOfPictures);
 
-function createGalleryMarkup(array){
-return array.map(({preview,original,description})=>
-`<a class="gallery__item" href="${original}">
+function createGalleryMarkup(array) {
+  return array
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`).join('');
-
+</a>`
+    )
+    .join('');
 }
-let lightbox = new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
   captionDelay: 250,
 });
